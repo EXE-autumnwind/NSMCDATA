5c004afb4cfad9f6c3a497b4c24069ef941675d0
luyii-code-1 <112700386+luyii-code-1@users.noreply.github.com>
2025-06-20 18:02:03 +0800

    New function & optimize

---
+++ radar-chinaall/README.md
@@ -0,0 +1,1 @@
+# 此API取自[http://nmc.cn/publish/radar/chinaall.html](http://nmc.cn/publish/radar/chinaall.html)
--- run.py
+++ run.py
@@ -5,29 +5,22 @@ import time
 
 
 os.makedirs('/Pull', exist_ok=True)
-
+#------------^^^^^^----------------
+#-------在这里修改储存路径----------
 while True:
-    # 获取当前UTC时间并减去20分钟
     utc_now = datetime.now(timezone.utc) - timedelta(minutes=18)
-
-    # 格式化URL参数（使用UTC时间）
     year = utc_now.strftime("%Y")
     month = utc_now.strftime("%m")
     day = utc_now.strftime("%d")
     time_str = utc_now.strftime("%Y%m%d%H%M00")
-
-    # 构建图片URL
     url = f"http://image.nmc.cn/product/{year}/{month}/{day}/RDCP/medium/SEVP_AOC_RDCP_SLDAS3_ECREF_ACHN_L88_PI_{time_str}000.PNG"
-
-    # 获取当前时间（UTC+8）用于保存文件名
     local_now = utc_now + timedelta(hours=8)
     print(f"Downloading from URL: {url}")
 
-    # 下载图片
     response = requests.get(url)
     if response.status_code == 200:
-        # 重命名文件为保存时间（UTC+8）
         filename = f"/Pull/{local_now.strftime('%Y%m%d%H%M%S')}.PNG"
+#--------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^------------这里可以改文件名称策略        
         try:
             with open(filename, "wb") as file:
                 file.write(response.content)
@@ -35,7 +28,6 @@ while True:
         except Exception as e:
             print(f"保存文件时出错: {e}")
     else:
-        print("下载失败！状态码:", response.status_code)
+        print("下载失败，有可能是当前未更新图片，属于正常情况，如果超过10分钟为正常获取图片，请进行进一步检查！状态码:", response.status_code)
 
-    # 等待6分钟
-    time.sleep(60)  # 360秒 = 6分钟
+    time.sleep(60)
